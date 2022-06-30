8ba139dbba06ddbc2aa9621b48c4541f43b88020
yexinlei <xinlei_ye@163.com>
2022-06-30 18:00:16 +0800

    refactor argParser

--- src/main/java/tdd/args/Args.java
+++ src/main/java/tdd/args/Args.java
@@ -26,7 +26,7 @@ public class Args {
 
     private static Map<Class<?>, OptionParser> PARSERS = Map.of(
             boolean.class, new BooleanParser(),
-            int.class, new IntOptionParser(),
-            String.class, new StringOptionParser()
+            int.class, new SingleValueOptionParser<>(Integer::parseInt),
+            String.class, new SingleValueOptionParser<>(String::valueOf)
     );
 }
